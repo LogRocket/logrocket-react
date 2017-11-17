a16be0130a17e14c85fa6e54648f4d937252c450
Jordan Sexton <jordan@jordansexton.com>
2017-11-16 19:40:13 -0600

    react-dom/lib/* no longer exists

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import hub from 'react-dom/lib/EventPluginHub';
+import { __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED } from 'react-dom';
+
+const hub = __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.EventPluginHub;
 
 export default function setupReact() {
   hub.injection.injectEventPluginsByName({
