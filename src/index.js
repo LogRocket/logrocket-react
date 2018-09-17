a43e6036c5b54696a772b7f63f98281ec2a4b4a5
Kaelan Cooter <dcgamers@gmail.com>
2018-09-17 17:58:03 -0400

    Add support for React 16.5 (#8)
    * fixed package for react 16.5
    
    * switch back to yarn
    
    * fixed install problems by switching from yarn to npm

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import { __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED } from 'react-dom';
 
-const hub = __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.EventPluginHub;
+
+// from https://github.com/facebook/react/blob/v16.5.1/packages/react-dom/src/client/ReactDOM.js#L750
+const injectEventPluginsByName = __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.Events[3];
 
 export default function setupReact() {
-  hub.injection.injectEventPluginsByName({
+  injectEventPluginsByName({
     ResponderEventPlugin: {
       extractEvents: (topLevelType, targetInst, nativeEvent) => {
         if (topLevelType !== 'click' || !targetInst) {
